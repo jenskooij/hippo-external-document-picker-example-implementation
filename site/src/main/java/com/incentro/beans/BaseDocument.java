ba1f6f407ef4ee532587be81a0ae9b48e07659cf
Jens Kooij <jens.kooij@incentro.com>
2017-04-13 12:47:32 +0200

    Initial commit

---
+++ site/src/main/java/com/incentro/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.incentro.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="externaldocumentpicker:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
